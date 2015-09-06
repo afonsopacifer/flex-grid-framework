e0385dfbaafc32acadd5efae8ac37556d9b4362a
afonsopacifer <afonsopolis@gmail.com>
2015-09-06 13:18:38 -0300

    css compress

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function( grunt ) {
     // stylus
     stylus: {
       options: {
-        compress: false,
+        compress: true,
       },
       compile: {
         files: {
